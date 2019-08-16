80bc5f128a24a7aceb87ff8f7fefb825b0c4af98
hemendrasethi <hemendra.sethi@welltok.com>
2019-08-16 21:24:54 +0530

    Fixed pagination Issue
    Fixed the Pagination Issue.
    this was dependent on thymleaf spring data

--- src/main/java/com/techsofin/sdccpayment/SdccpaymentApplication.java
+++ src/main/java/com/techsofin/sdccpayment/SdccpaymentApplication.java
@@ -2,12 +2,19 @@ package com.techsofin.sdccpayment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.dialect.springdata.SpringDataDialect;
 
 @SpringBootApplication
 public class SdccpaymentApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SdccpaymentApplication.class, args);
+    }
+
+    @Bean
+    public SpringDataDialect springDataDialect() {
+        return new SpringDataDialect();
     }
 
 }
