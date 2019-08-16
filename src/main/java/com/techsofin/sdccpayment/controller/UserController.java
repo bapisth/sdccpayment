737516dc62b67851720bf09a24f21a52f8fdb579
hemendrasethi <hemendra.sethi@welltok.com>
2019-08-16 21:34:54 +0530

    Delete User
    Delete User

--- src/main/java/com/techsofin/sdccpayment/controller/UserController.java
+++ src/main/java/com/techsofin/sdccpayment/controller/UserController.java
@@ -3,6 +3,7 @@ package com.techsofin.sdccpayment.controller;
 import com.techsofin.sdccpayment.entity.User;
 import com.techsofin.sdccpayment.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,6 +13,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 import java.util.Objects;
@@ -59,6 +61,30 @@ public class UserController {
         user.setPassword(encodePassword);
         user.setDeleted(false);
         userRepository.save(user);
+        return "redirect:/user/list";
+    }
+
+    @GetMapping("/delete")
+    public ModelMap deleteConfirm(@RequestParam(value = "id", required = true) User user ) {
+        return new ModelMap("user", user);
+    }
+
+
+
+
+    @PostMapping("/delete")
+    public Object delete(@ModelAttribute User user , SessionStatus status) {
+        try{
+            userRepository.delete(user);
+        } catch (DataIntegrityViolationException exception) {
+            status.setComplete();
+            return new ModelAndView("error/errorHapus")
+                    .addObject("entityId", user.getName())
+                    .addObject("entityName", "User")
+                    .addObject("errorCause", exception.getRootCause().getMessage())
+                    .addObject("backLink","/user/list");
+        }
+        status.setComplete();
         return "redirect:/user/list";
     }
 }
