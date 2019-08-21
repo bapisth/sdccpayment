6e7848db6a650f98290a4b4b2369d1be21b42b13
gargoyle008 <rishavgarg.008@gmail.com>
2019-08-21 13:27:38 +0530

    CustomerController and Repo

--- src/main/java/com/techsofin/sdccpayment/controller/CustomerController.java
+++ src/main/java/com/techsofin/sdccpayment/controller/CustomerController.java
@@ -1,8 +1,29 @@
 package com.techsofin.sdccpayment.controller;
 
+import java.awt.print.Pageable;
+import java.util.Objects;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.techsofin.sdccpayment.entity.Customer;
+import com.techsofin.sdccpayment.repository.CustomerRepository;
+
+import groovyjarjarpicocli.CommandLine.Model;
 
 @Controller
 @RequestMapping(value = "/customer")
@@ -10,9 +31,66 @@ public class CustomerController {
 	@Autowired
 	private CustomerRepository costumerRepository;
 	
-	@GetMapping(vlaue = "/listCustomer")
-	
+	/*@GetMapping(value = "/listCustomer")
+	public ModelMap list(@PageableDefault(size = 5) Pageable pageable, @RequestParam
+			(name = "value", required = false) String value, Model model) {
+        if (value != null) {
+           
+            return new ModelMap().addAttribute("customers", CustomerRepository.findByNameContainingIgnoreCase(value, pageable));
+        } else {
+            return new ModelMap().addAttribute("customers", CustomerRepository.findAll(pageable));
+        }
+    }*/
+
+    @GetMapping("/formCustomer")
+    public ModelMap showCustomer(@RequestParam(value = "id", required = false) Customer customer) {
+        if (customer == null) {
+            customer = new Customer();
+        }
+        return new ModelMap("customer", customer);
+    }
+
+    @PostMapping(value = "/formCustomer")
+    public String submit(@Valid @ModelAttribute Customer customer, BindingResult errors, SessionStatus status, Model model) {
+
+        // if the form has any errors
+        if (errors.hasErrors()) {
+            return "customer/createCustomer";
+        }
+
+       
+
+    
+        return "redirect:/customer/listCustomer";
+    }
+
+    @GetMapping("/deleteCustomer")
+    public ModelMap deleteConfirm(@RequestParam(value = "id", required = true) Customer customer ) {
+        return new ModelMap("customer", customer);
+    }
+}
+
+
+
+
+   /* @PostMapping("/deleteCustomer")
+    public Object delete(@ModelAttribute Customer customer , SessionStatus status) {
+        try{
+           CustomerRepository.delete(ustomer);
+        } catch (DataIntegrityViolationException exception) {
+            status.setComplete();
+            return new ModelAndView("error/errorHapus")
+                    .addObject("entityId", customer.getCustomerName())
+                    .addObject("entityName", "Customer")
+                    .addObject("errorCause", exception.getRootCause().getMessage())
+                    .addObject("backLink","/customer/listCustomer");
+        }
+        status.setComplete();
+        return "redirect:/customer/listCustomer";
+    }
+}
+	*/
 	
 	
 
-}
+
--- src/main/java/com/techsofin/sdccpayment/repository/CustomerRepository.java
+++ src/main/java/com/techsofin/sdccpayment/repository/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.techsofin.sdccpayment.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.techsofin.sdccpayment.entity.Customer;
@@ -12,8 +14,20 @@ import com.techsofin.sdccpayment.entity.Customer;
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 	
 	Customer findByName(String customerName);
+	
+	Customer findByCustomername(String name);
 
-    Customer findByMobileAndName(String mobileNumber, String customerName);
+
+
+    List<Customer> findByNameLikeAndCustomernameEquals(String name, String customerName);
+
+    List<Customer> findByNameLikeAndCustomerNameLike(String name, String customerName);
+
+    Page<Customer> findByNameContainingIgnoreCase(String name, Pageable pageable);
+
+    
+	
+	Customer findByMobileAndName(String mobileNumber, String customerName);
 
     Customer findByNameAndMobile(String customerName, String mobileNumber);
 
